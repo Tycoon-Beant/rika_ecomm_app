010bf6af9c78a80c54c56590660081c894552a50
taran6859 <tarandeep.singh@genboot.com>
2024-10-24 16:37:13 +0530

    initial commit

---
+++ lib/screens/categorie_screen.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:rika_ecomm_app/models/category_model.dart';
+
+class Categories extends StatefulWidget {
+  const Categories({super.key});
+
+  @override
+  State<Categories> createState() => _CategoriesState();
+}
+
+class _CategoriesState extends State<Categories> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          leading: Image.asset("assets/images/arrow.png"),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 10),
+              child: Image.asset("assets/images/search.png"),
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Categories',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 600,
+                child: ListView.builder(
+                  itemCount: categoryContents.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Column(
+                      children: [
+                        Container(
+                          height: 70,
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(80),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(20),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Image.asset(categoryContents[index].image),
+                                SizedBox(
+                                  width: 15,
+                                ),
+                                SizedBox(
+                                  width: 150,
+                                  child: Text(
+                                    categoryContents[index].category,
+                                    style: const TextStyle(
+                                        fontSize: 18,
+                                        fontFamily: 'Mont Blanc Bold',
+                                        color: Colors.white),
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 100,
+                                  child: Text(
+                                    categoryContents[index].products,
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: 'Mont Blanc Regular',
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        )
+                      ],
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/payment_confirm_screen.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+
+class PaymentConfirmScreen extends StatefulWidget {
+  const PaymentConfirmScreen({super.key});
+
+  @override
+  State<PaymentConfirmScreen> createState() => _PaymentConfirmScreenState();
+}
+
+class _PaymentConfirmScreenState extends State<PaymentConfirmScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        surfaceTintColor: Colors.white,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 10, top: 10),
+          child: Image.asset('assets/images/arrow.png'),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 18),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Payment',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: 400,
+                child: Image.asset('assets/images/card.png'),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Card Details',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Card number',
+                  hintStyle: TextStyle(color: Color(0xff888888), fontSize: 15),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey.shade100,
+                      ),
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Exp date',
+                  hintStyle: TextStyle(color: Color(0xff888888), fontSize: 15),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey.shade100,
+                      ),
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'CVV',
+                  hintStyle: TextStyle(color: Color(0xff888888), fontSize: 15),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey.shade100,
+                      ),
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Cancel',
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w800,
+                        color: Color(0xffAAAAAA)),
+                  ),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+                      ),
+                      // onPressed: () => Navigator.push(
+                      //     context,
+                      // MaterialPageRoute(
+                      //     builder: (context) => const Succesfulscreen())),
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                                backgroundColor: Colors.white,
+                                content: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Image.asset(
+                                        "assets/images/successicon.png"),
+                                    Text(
+                                      "Successfull",
+                                      style: TextStyle(
+                                          fontSize: 26,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      "You have successfully your shopping cart list!",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          fontSize: 16, color: Colors.grey),
+                                    ),
+                                    const SizedBox(height: 20),
+                                    ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                          backgroundColor: const Color.fromARGB(
+                                              255, 0, 0, 0),
+                                        ),
+                                        onPressed: () {},
+                                        child: Text(
+                                          "continue Shopping",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 18),
+                                        ))
+                                  ],
+                                ));
+                          },
+                        );
+                      },
+                      child: Text(
+                        "Confirm",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 255, 255, 255),
+                        ),
+                      )),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
